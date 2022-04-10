d1fece8216ad531ff22a6a608bf1535933114edb
Brandon Harris <namebrandon@users.noreply.github.com>
2022-04-10 14:58:58 -0500

    formatting

--- r5c_patch.py
+++ r5c_patch.py
@@ -2,21 +2,24 @@
 # Author:		Brandon Harris
 # Email:		brandon.harris@gmail.com
 # Date:			2022/04/10
+#
+#
 # Description:	This script iterates on every .CR3 file in the present directory
-#				and overwites the camera model name (R5 C) with the original R5
-#				model name. This will impact EXIF and metadata.
-
-#				WARNING! This program overwites your original image data.
-#				PLEASE make a copy of the images before modifying them.
-#				Images are overwritten and saved in place.
-
-# License:		None. Modify and use at will.
+#		and overwites the camera model name (R5 C) with the original R5
+#		model name. This will impact EXIF and metadata.
+#		
+#		WARNING! This program overwites your original image data.
+#
+#		PLEASE make a copy of the images before modifying them.
+#		Images are overwritten and saved in place.
+#
+# License:	None. Modify and use at will.
 # Disclaimer:	I am not responsible for any use, misuse or other impact of
-#				executing this code. I provide no warranty, implied, fit for purpose
-#				or otherwise.
+#		executing this code. I provide no warranty, implied, fit for purpose
+#		or otherwise.
 
 # Usage:		save python script to directory with .CR3 images. open a command prompt
-#				or terminal shell and execute the following
+#			or terminal shell and execute the following
 #					 python3 r5c_patch.py
 
 
